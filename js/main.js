0d42a5d5e99759a0b9a769d6c1affdef76a6be8a
Harrison <Isaiah_Harrison@comcast.com>
2017-12-09 01:31:13 -0500

    the two seperate sites

--- js/main.js
+++ js/main.js
@@ -49,7 +49,8 @@ app.controller("main", ["$scope", "$rootScope", "$timeout", "$interval", functio
     }
 
   }
-  $scope.randomBoxColor;
+  $scope.randomBoxColor = '#5C00BB';
+  $scope.landingPage = true;
   $scope.boxes = [1,   2,  3,  4,  5,  6,  7,  8,  9, 10,
                   11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                   21, 22, 23, 24, 25];
@@ -71,6 +72,27 @@ app.controller("main", ["$scope", "$rootScope", "$timeout", "$interval", functio
   }
   $scope.resetHighlight = () => {
     $(".colorBox").css("backgroundColor", "");
+  }
+
+  if($scope.landingPage){
+    $timeout(() => {
+      $(".colorBoxHolder").show();
+      $scope.randomHighlight(1, 25);
+      $scope.randomHighlight(1, 25);
+      $scope.randomHighlight(1, 25);
+      $scope.randomHighlight(1, 25);
+    }, 1200);
+    const landingChangingBackground = $interval(function () {
+      if($scope.homepageCheckVar){
+        $scope.resetHighlight();
+        $scope.randomHighlight(1, 25);
+        $scope.randomHighlight(1, 25);
+        $scope.randomHighlight(1, 25);
+        $scope.randomHighlight(1, 25);
+      }
+    }, 6000);
+  } else {
+    $interval.cancel(landingChangingBackground);
   }
 
   let choiceMade = false;
